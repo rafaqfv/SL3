4f4176becb80b1519630a7d91bc8796984dfc7b2
Rafael <127614067+rafaqfv@users.noreply.github.com>
2023-09-13 20:04:09 -0300

    Alguns ajustes

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ ScrollReveal({ reset: true });
 
 ScrollReveal().reveal('.licapa', { delay: 200 });
 ScrollReveal().reveal('.imagens', { delay: 100 });
+ScrollReveal().reveal('span', { delay: 100 });
